f8d2983ac954a68ddde6b22f4f80d8bd4c985e5f
Pablo H. F. Roscoche <pablo.dkz2@gmail.com>
2023-02-14 09:52:19 -0300

    Added home page layout and styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,24 @@
 import Head from "next/head";
 
+import styles from "./home.module.scss";
+
 export default function Home() {
   return (
     <>
       <Head>
         <title>Minimum</title>
       </Head>
-      <h1>Hello</h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <h1>Stay Curious.</h1>
+          <p>
+            Get access to stories, essays and expertise from writers on any
+            topic <br />
+            <span>for $9.99 monthly</span>
+          </p>
+        </section>
+      </main>
     </>
   );
 }
