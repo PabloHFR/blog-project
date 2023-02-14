b417a3c280acdf736f6af8c9e1e1eafd7afb9ca4
Pablo H. F. Roscoche <pablo.dkz2@gmail.com>
2023-02-14 08:08:01 -0300

    Created next-app and added sass

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+import styles from "./styles/home.module.scss";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Minimum</title>
+      </Head>
+      <h1 className={styles.title}>Hello</h1>;
+    </>
+  );
+}
