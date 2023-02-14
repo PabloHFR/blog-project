0973d7da9e08d1a8d357e99c78a0c8a5d8477769
Pablo H. F. Roscoche <pablo.dkz2@gmail.com>
2023-02-14 08:18:05 -0300

    Added global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
 import Head from "next/head";
 
-import styles from "./styles/home.module.scss";
-
 export default function Home() {
   return (
     <>
       <Head>
         <title>Minimum</title>
       </Head>
-      <h1 className={styles.title}>Hello</h1>;
+      <h1>Hello</h1>;
     </>
   );
 }
