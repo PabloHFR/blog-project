42895e1a3e322f8106165edcf2d809d4f2a7bf00
Pablo H. F. Roscoche <pablo.dkz2@gmail.com>
2023-02-16 08:06:36 -0300

    Added faunadb database and create new user feature

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,10 @@
-import NextAuth from "next-auth";
+import { query as q } from "faunadb";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
-export const authOptions = {
+import { fauna } from "../../../services/fauna";
+
+export const authOptions: NextAuthOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID!,
@@ -13,5 +16,23 @@ export const authOptions = {
       },
     }),
   ],
+  jwt: {
+    secret: process.env.SIGNING_KEY!,
+  },
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const userEmail = user.email;
+
+      try {
+        await fauna.query(
+          q.Create(q.Collection("users"), { data: { userEmail } })
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 };
 export default NextAuth(authOptions);
