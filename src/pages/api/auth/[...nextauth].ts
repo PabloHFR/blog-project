ec45cdaed7f15e25418181bd50ccee34cba2bcf3
Pablo H. F. Roscoche <pablo.dkz2@gmail.com>
2023-02-16 09:28:30 -0300

    Added check if user already exists

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -16,16 +16,29 @@ export const authOptions: NextAuthOptions = {
       },
     }),
   ],
-  jwt: {
-    secret: process.env.SIGNING_KEY!,
-  },
   callbacks: {
     async signIn({ user, account, profile }) {
-      const userEmail = user.email;
+      const { email } = user;
 
       try {
         await fauna.query(
-          q.Create(q.Collection("users"), { data: { userEmail } })
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index("user_by_email"),
+                  q.Casefold(user.email as string)
+                )
+              )
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(
+              q.Match(
+                q.Index("user_by_email"),
+                q.Casefold(user.email as string)
+              )
+            )
+          )
         );
 
         return true;
