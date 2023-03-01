b7401dab314b5eed2cb6d90870a1724406ce7adb
Pablo H. F. Roscoche <pablo.dkz2@gmail.com>
2023-02-28 21:00:00 -0300

    Added navigation to posts page and active post anchor element styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,27 @@
+import { useRouter } from "next/router";
+import Link from "next/link";
+
 import styles from "./styles.module.scss";
 
 import { SignInButton } from "../SignInButton";
 
 export function Header() {
+  const { asPath } = useRouter();
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <h1>Minimum</h1>
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <Link href="/" className={asPath === "/" ? styles.active : ""}>
+            Home
+          </Link>
+          <Link
+            href="/posts"
+            className={asPath === "/posts" ? styles.active : ""}
+          >
+            Posts
+          </Link>
         </nav>
         <SignInButton />
       </div>
